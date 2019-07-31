607966dfbd13b34ad19d7028fa92bd23d80b3eb7
PsChina <panshanzxcv.love@163.com>
2019-07-31 16:03:19 +0800

    Perfect the types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import './vue';
 
-type NumberDate = number | string;
-type WeekConfig = 'cn' | 'en' | DateOption;
-type DateData = NumberDate | Date;
+export type NumberDate = number | string;
+export type WeekConfig = 'cn' | 'en' | DateOption;
+export type DateData = NumberDate | Date;
 
-interface CurrencyOption {
+export interface CurrencyOption {
   [key: string]: any;
   symbolOnLeft: boolean;
   separator: string;
@@ -14,49 +14,49 @@ interface CurrencyOption {
   round: boolean;
 }
 
-interface DateOption {
+export interface DateOption {
   [key: string]: string[];
   week: string[];
   shortWeek: string[];
 }
 
-interface DateOptions {
+export interface DateOptions {
   [key: string]: DateOption;
   cn: DateOption;
   en: DateOption;
 }
 
-type Comparator = (item1: any, item2: any, key: string, reverse?: boolean) => number;
+export type Comparator = (item1: any, item2: any, key: string, reverse?: boolean) => number;
 
-type MatchFunction = (val: any) => boolean;
+export type MatchFunction = (val: any) => boolean;
 
-type Match = string | MatchFunction;
+export type Match = string | MatchFunction;
 
-interface MatchRules {
+export interface MatchRules {
    match: Match;
    ignore?: string[] | string;
 }
 
-interface SignOption {
+export interface SignOption {
   zero: string;
 }
 
-type Sign = SignOption | boolean;
-interface NumberOptions {
+export type Sign = SignOption | boolean;
+export interface NumberOptions {
   round?: boolean;
   pad?: boolean;
   sign?: Sign;
   separator?: string;
 }
 
-interface LimitToOption {
+export interface LimitToOption {
   startWithIndex: number;
   startWith?: any;
   ignore?: string | RegExp;
   cutOut?: boolean;
 }
 
-type FilterOptions = MatchRules | Match | RegExp;
+export type FilterOptions = MatchRules | Match | RegExp;
 
 export function install(vue: typeof Vue): void;
 
@@ -90,3 +90,31 @@ export declare interface EasyFilter {
 
   lowercase(input: string, start?: number, end?: number): string
 }
+
+export function currency(input: NumberDate, symbol?: string, digits?: number, options?: CurrencyOption ): string;
+
+export function date(input: DateData, formatMode?: string, option?: WeekConfig): DateData;
+
+export function orderBy(input: any[],
+                        expression?: Comparator | string,
+                        reverse?: boolean,
+                        comparator?: Comparator | string,
+                        ): any[];
+
+export function filter(input: any, matchOptions?: FilterOptions): any;
+
+export function number(
+                        input: NumberDate,
+                        digits?: number,
+                        options?: NumberOptions,
+                      ): string;
+
+export function limitTo(
+  input: number | string | any[],
+  limit?: number,
+  option?: LimitToOption,
+): string | number | any[];
+
+export function uppercase(input: string, start?: number, end?: number): string;
+
+export function lowercase(input: string, start?: number, end?: number): string;
